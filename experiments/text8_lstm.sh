3b1c3b7241a166a90c96000724d39146801dbbb4
Nicolas Ballas <ballas.n@gmail.com>
2016-04-11 10:18:44 -0400

    update script

--- experiments/text8_lstm.sh
+++ experiments/text8_lstm.sh
@@ -1,6 +1,6 @@
 command="../text8.py --length 180 --initialization orthogonal --optimizer adam --learning-rate 0.001 --batch-size 128 --num-epochs 50 --baseline"
 nh=2000
-directory=bn-lstm-nh$nh
+directory=text8-lstm-nh$nh
 mkdir -p $directory
 cd $directory
 ipython --pdb $command --num-hidden $nh
