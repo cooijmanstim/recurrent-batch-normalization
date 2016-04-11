f7c3fe05985ba416b64d8c7cadd9e9331e9daa2e
Nicolas Ballas <ballas.n@gmail.com>
2016-04-11 10:03:58 -0400

    add text8 experiments hyperparameters

---
+++ experiments/text8_lstm.sh
@@ -0,0 +1,7 @@
+command="../text8.py --length 180 --initialization orthogonal --optimizer adam --learning-rate 0.001 --batch-size 128 --num-epochs 50 --baseline"
+nh=2000
+directory=bn-lstm-nh$nh
+mkdir -p $directory
+cd $directory
+ipython --pdb $command --num-hidden $nh
+cd ..
