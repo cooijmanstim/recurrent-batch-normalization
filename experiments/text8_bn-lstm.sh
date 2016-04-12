03bbadf71a71afb3d0aedf8c1e448b5a7f431a7c
Nicolas Ballas <ballas.n@gmail.com>
2016-04-12 15:42:10 -0400

    update text8

--- experiments/text8_bn-lstm.sh
+++ experiments/text8_bn-lstm.sh
@@ -1,7 +1,7 @@
-command="../text8.py --length 180 --initialization orthogonal --optimizer adam --learning-rate 0.001 --batch-size 128 --num-epochs 50"
+command="../../text8.py --length 180 --initialization orthogonal --optimizer adam --learning-rate 0.001 --batch-size 128 --num-epochs 50"
 nh=2000
 directory=text8-bn-lstm-nh$nh
 mkdir -p $directory
 cd $directory
-ipython --pdb $command --num-hidden $nh
+python $command --num-hidden $nh
 cd ..
--- experiments/text8_lstm.sh
+++ experiments/text8_lstm.sh
@@ -1,7 +1,7 @@
-command="../text8.py --length 180 --initialization orthogonal --optimizer adam --learning-rate 0.001 --batch-size 128 --num-epochs 50 --baseline"
+command="../../text8.py --length 180 --initialization orthogonal --optimizer adam --learning-rate 0.001 --batch-size 128 --num-epochs 50 --baseline"
 nh=2000
 directory=text8-lstm-nh$nh
 mkdir -p $directory
 cd $directory
-ipython --pdb $command --num-hidden $nh
+python $command --num-hidden $nh
 cd ..
