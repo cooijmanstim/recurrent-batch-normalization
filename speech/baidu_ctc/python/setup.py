35f3a456dbba37a0a63aebe1d5c6b33f2fd9a6c0
Tim Cooijmans <cooijmans.tim@gmail.com>
2016-05-20 12:56:39 -0700

    remove speech leftovers

--- speech/baidu_ctc/python/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-setup(name='ctc',
-      version='0.1',
-      packages=['ctc'],
-      package_dir={'ctc': '.'},
-      package_data={'ctc' : ['../build/libwarpctc.*']}
-      )
